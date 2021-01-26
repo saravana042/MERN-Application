49cc94b07575afc48679239f92b813cde4b0992d
saravana042 <usaravanankarthick@gmail.com>
2021-01-26 13:01:52 +1100

    Adding a UserItem Component

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-    return <h2>User Works!</h2>
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Sara',
+            image: 'https://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Female-Face-FG-5-brunette-icon.png',
+            places: 3
+        }
+    ]
+    return <UsersList items={USERS} />;
 }
 
 export default Users;
