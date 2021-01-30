4f4d60be6c60a3509ef3b440965d39dc18a2f86c
saravana042 <usaravanankarthick@gmail.com>
2021-01-30 11:29:17 +1100

    Rendering User Places & Using Dynamic route segments

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -21,12 +21,11 @@ const MainNavigation = props => {
   return (
     <React.Fragment>
       {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
-
-        <SideDrawer show ={drawerIsOpen} onClick={closeDrawerHandler}>
-          <nav className="main-navigation__drawer-nav">
-            <NavLinks />
-          </nav>
-        </SideDrawer>
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
 
       <MainHeader>
         <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
