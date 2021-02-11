1150279dd6bdded7a6bdc0bb7d5ba0703db2772e
saravana042 <usaravanankarthick@gmail.com>
2021-02-11 19:04:36 +1100

    Finishing the Add place Form

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -48,8 +48,13 @@ const NewPlace = () => {
     dispatch({type: 'INPUT_CHANGE', value: value, isValid: isValid, inputId: id });
   }, []);
 
+  const placeSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  }
+
   return (
-  <form className="place-form">
+  <form className="place-form" onSubmit={placeSubmitHandler}>
     <Input 
       element="input"
       id="title"
@@ -65,6 +70,14 @@ const NewPlace = () => {
       label="Description" 
       validators={[VALIDATOR_MINLENGTH(5)]}
       errorText="Please enter a valid description (at least 5 characters)"
+      onInput={inputHandler}
+    />
+    <Input 
+      id="address"
+      element="input"
+      label="Address" 
+      validators={[VALIDATOR_REQUIRE()]}
+      errorText="Please enter a valid address"
       onInput={inputHandler}
     />
     <Button type="submit" disabled={!formState.isValid}> ADD PLACE </Button>
