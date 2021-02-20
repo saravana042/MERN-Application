21b046d54329ec00afe3f44b82c756774cd958e0
saravana042 <usaravanankarthick@gmail.com>
2021-02-21 10:46:51 +1100

    Handling errors -  backend

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -29,19 +29,35 @@ const DUMMY_PLACES = [
 
 router.get('/:pid', (req, res, next) => {
     const placeId = req.params.pid; // { pid: 'p1' }
+
     const place = DUMMY_PLACES.find(p => {
         return p.id === placeId;
-    })
+    });
     //console.log("GET Request in Places");
+
+    if (!place) {
+        const error = new Error('Could not find a place for the provided id.');
+        error.code = 404;
+        throw error;
+    }
+
     res.json({ place }); // => { place } => { place: place }
 });
 
 router.get('/user/:uid', (req, res, next) => {
     const userId = req.params.uid; // { uid: 'u1 }
+
     const place = DUMMY_PLACES.find(p => {
         return p.creator === userId;
-    })
-    res.json({ place })
+    });
+
+    if (!place) {
+        const error = new Error('Could not find a place for the provided user id.');
+        error.code = 404;
+        return next(error);
+    }
+
+    res.json({ place });
 });
 
 module.exports = router;
