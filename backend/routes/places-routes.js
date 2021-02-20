f77874e5ee067ee7f20e4f3037475cb06a2442b8
saravana042 <usaravanankarthick@gmail.com>
2021-02-20 18:19:49 +1100

    Getting a place By User ID

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -14,15 +14,34 @@ const DUMMY_PLACES = [
         },
         creator: 'u1'
     },
+    {
+        id: 'p2',
+        title: 'Taj mahal',
+        description: 'One of the most famous sky scrapers in the world!',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u2'
+    }
 ]
 
 router.get('/:pid', (req, res, next) => {
-    const placeId = req.params.pid; { pid: 'p1' }
+    const placeId = req.params.pid; // { pid: 'p1' }
     const place = DUMMY_PLACES.find(p => {
         return p.id === placeId;
     })
-    console.log("GET Request in Places");
+    //console.log("GET Request in Places");
     res.json({ place }); // => { place } => { place: place }
+});
+
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid; // { uid: 'u1 }
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId;
+    })
+    res.json({ place })
 });
 
 module.exports = router;
