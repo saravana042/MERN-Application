a89c40cb422a6bef76a411324c7a31871399d1f7
saravana042 <usaravanankarthick@gmail.com>
2021-03-11 23:05:15 +1100

    Creating & Storing Documents in the Database

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -3,6 +3,7 @@ const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
 const getCoordsForAddress = require('../util/location');
+const Place = require('../models/place');
 
 let DUMMY_PLACES = [
     {
@@ -82,16 +83,25 @@ const createPlace = async (req, res, next) => {
         return next(error);
     }
 
-    const createdPlace = {
-        id: uuidv4(),
+    const createdPlace = new Place({
         title,
         description,
-        location: coordinates,
         address,
+        location: coordinates,
+        image: 'https://image.freepik.com/free-vector/cute-penguin-flying-with-balloons-cartoon-vector-illustration-animal-love-concept-isolated-vector-flat-cartoon-style_138676-2016.jpg',
         creator
-    };
+    });
 
-    DUMMY_PLACES.push(createdPlace);  //unshift(createdPlace)
+    // DUMMY_PLACES.push(createdPlace);  //unshift(createdPlace)
+
+    try {
+        await createdPlace.save();
+    } catch (err) {
+        const error = new HttpError(
+            'Creating place failed, Please try again.', 500
+        );
+        return next(error);
+    }
 
     res.status(201).json({ place: createdPlace });
 };
