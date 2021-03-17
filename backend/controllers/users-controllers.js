6c5f160d0898844cacad4a548251dbc418329f22
saravana042 <usaravanankarthick@gmail.com>
2021-03-17 19:32:23 +1100

    Adding the User Login

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -47,8 +47,6 @@ const signup = async (req, res, next) => {
         return next(error);
     }
 
-
-
     if (existingUser) {
         const error = new HttpError(
             'User exist already, please login instead.', 422
@@ -78,12 +76,29 @@ const signup = async (req, res, next) => {
     res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
     const { email, password } = req.body;
 
-    const identifiedUser = DUMMY_USERS.find(u => u.email === email);
-    if (!identifiedUser || identifiedUser.password !== password) {
-        throw new HttpError('Could not identify user, credentials seem to be wrong.', 401);
+    // const identifiedUser = DUMMY_USERS.find(u => u.email === email);
+    // if (!identifiedUser || identifiedUser.password !== password) {
+    //     throw new HttpError('Could not identify user, credentials seem to be wrong.', 401);
+    // }
+
+    let existingUser
+    try {
+        existingUser = await User.findOne({ email: email })
+    } catch (err) {
+        const error = new HttpError(
+            'Signing up login, please try again later.', 500
+        );
+        return next(error);
+    }
+
+    if (!existingUser || existingUser.password !== password) {
+        const error = new HttpError(
+            'Invalid credentials, could not log you in.', 401
+        );
+        return next(error);
     }
 
     res.json({ message: 'Logged in!' });
