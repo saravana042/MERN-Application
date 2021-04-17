18cf1b057f570439ba430e54c4557df193db5599
saravana042 <usaravanankarthick@gmail.com>
2021-04-17 12:49:29 +1000

    Generating JWT token in backend

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
@@ -88,7 +89,23 @@ const signup = async (req, res, next) => {
         return next(error);
     }
 
-    res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+    // Jwt token
+
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: createdUser.id, email: createdUser.email },
+            'supersecret_dont_share',
+            { expiresIn: '1h' }
+        );
+    } catch (err) {
+        const error = new HttpError(
+            'Signing Up failed, Please try again.', 500
+        );
+        return next(error);
+    }
+
+    res.status(201).json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 const login = async (req, res, next) => {
@@ -133,9 +150,24 @@ const login = async (req, res, next) => {
         return next(error);
     }
 
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: existingUser.id, email: existingUser.email },
+            'supersecret_dont_share',
+            { expiresIn: '1h' }
+        );
+    } catch (err) {
+        const error = new HttpError(
+            'Logging Up failed, Please try again.', 500
+        );
+        return next(error);
+    }
+
     res.json({
-        message: 'Logged in!',
-        user: existingUser.toObject({ getters: true })
+        userId: existingUser.id,
+        email: existingUser.email,
+        token: token
     });
 };
 
