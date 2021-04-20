e1500aaf94cf2b81e8f82ad407d7023aa05161cf
saravana042 <usaravanankarthick@gmail.com>
2021-04-20 22:18:22 +1000

    Enviroment variable in React App

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -39,7 +39,7 @@ const NewPlace = () => {
   const placeSubmitHandler = async event => {
     event.preventDefault();
     try {
-      await sendRequest('http://localhost:5000/api/places', 'POST', JSON.stringify({
+      await sendRequest(process.env.REACT_APP_BACKEND_URL + '/places', 'POST', JSON.stringify({
         title: formState.inputs.title.value,
         description: formState.inputs.description.value,
         address: formState.inputs.address.value
